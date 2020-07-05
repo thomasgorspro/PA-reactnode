3345ef0d65c702162e684ba05b3da86def77800b
Florian Durand <florian-94@hotmail.fr>
2020-07-05 11:15:36 +0200

    first throw

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -19,8 +19,14 @@ router
     });
   })
 
+  .get("/orders/:id", (req, res) => {
+    Transaction.findById(req.params.id).then((data) => {
+        res.render("orderView", { order: data })
+    })
+  })
+
   .get("/login", (req, res) => {
     res.render("login");
-  })
+  });
 
 export default router;
